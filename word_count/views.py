de780136bade4e695deb807662858454bafa6fda
Tony Truong <tony888t@googlemail.com>
2020-06-07 20:09:35 +0100

    Added more comments.

--- word_count/views.py
+++ word_count/views.py
@@ -12,6 +12,12 @@ from django.views import View
 
 class Words(View):
     def extract_keywords(self, text):
+        """
+        Extracts keyword from given text based on gensim word scoring.
+        Set ratio to 0.1 to return highest scoring keywords.
+
+        return a list of keywords.
+        """
         return keywords(text, split=True, ratio=0.1)
 
     def process_files(self, file_path=None, file_type='txt'):
@@ -20,15 +26,17 @@ class Words(View):
         if not file_path:
             file_path = os.path.join(settings.BASE_DIR, 'word_count/default_docs')
 
+        # Find all files in a given directory.
         documents = [doc for doc in glob.glob(file_path + f"**/*.{file_type}", recursive=True)]
 
         for doc in documents:
             with open(doc, 'r') as doc_file:
+                # Remove newlines from text
                 file_text = doc_file.read().replace('\n', '')
                 file_keywords = self.extract_keywords(file_text)
 
                 for word in file_keywords:
-                    # TODO: Need to find out why gensim keyword extracts and adds suffixs to words. let and letting
+                    # TODO: Need to find out why gensim keyword extracts and adds suffixes to words. let and letting
                     word_count = file_text.lower().count(word.lower())
                     if word_count > 0:
                         if counter.get(word):
